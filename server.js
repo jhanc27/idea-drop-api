7418f4046ce9f3b588b3e2ef8f1e131761e782aa
Jhan Carlos <yanmarte27@gmail.com>
2026-05-12 06:11:36 -0400

    Added prod domain

--- server.js
+++ server.js
@@ -14,7 +14,10 @@ const PORT = process.env.PORT || 8000;
 
 connectDB();
 
-const allowedOrigins = ['http://localhost:3000'];
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://idea-drop-ui-mauve-mu.vercel.app',
+];
 
 app.use(
   cors({
